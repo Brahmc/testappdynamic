fe63f392e7ce42509ab2b3fc5fbf2ec99f7be4ae
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-09-20 15:19:53 +0200

    fixed product negation

--- src/components/MenuProduct.js
+++ src/components/MenuProduct.js
@@ -1,5 +1,5 @@
 export function MenuProduct(props) {
     const {product} = props;
-    if(product?.name) return;
+    if(!product?.name) return;
     return <div className={'menuProduct'}><div>{product.name}</div><div>{product.price}</div></div>
 }
