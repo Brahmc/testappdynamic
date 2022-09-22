a2f2a13f78cbbc3867b1a2659391b07b77f3dae2
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-09-22 09:19:40 +0200

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import 'normalize.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import {PRODUCTS_DATA} from "./data/data";
 import {MenuCardPage} from "./pages/MenuCardPage";
 import {PicturesPage} from "./pages/PicturesPage";
 
 function App() {
-    return (<>
-        <MenuCardPage products={PRODUCTS_DATA} />
-        <PicturesPage/>
-        </>
+    return (
+        <Tabs>
+            <TabList>
+                <Tab>Title 1</Tab>
+                <Tab>Title 2</Tab>
+            </TabList>
+            <TabPanel>
+                <MenuCardPage products={PRODUCTS_DATA} />
+            </TabPanel>
+            <TabPanel>
+                <PicturesPage/>
+            </TabPanel>
+        </Tabs>
     );
 }
 export default App;
