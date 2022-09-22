032ff025f3f5cd907f0f6fdd5c235ce3c6935f58
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-09-22 12:37:00 +0200

    number + car function refactor

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,6 +1,7 @@
 import Col from "react-bootstrap/Col";
 import {Card} from "react-bootstrap";
 import {Section} from "./Section";
+import * as PropTypes from "prop-types";
 
 const colors = [
     ['blauw', 'blue'],
@@ -12,21 +13,24 @@ const colors = [
 ];
 const colorMap = new Map(colors);
 
+function Car(props) {
+    const {car} = props;
+    return <Col className="g-3" xs={3}>
+        <Card>
+            <h1>{car.name}</h1>
+            {car.brand ? <span>merk: {car.brand}</span> : null}
+            {car.type ? <span>type: {car.type}</span> : null}
+            {car.color ? <span style={{
+                background: colorMap.get(car.color),
+                color: colorMap.get(car.color) === "black" ? "white" : ""
+            }}>kleur: {car.color}</span> : null}
+        </Card>
+    </Col>;
+}
+
+Car.propTypes = {car: PropTypes.any};
+
 export function Cars(props) {
     const {cars, title} = props;
-    return (
-        <Section
-            title={title}
-            content={
-            cars.map(car =>
-            <Col className='g-3' xs={3} key={car.id}>
-                <Card >
-                    <h1>{car.name}</h1>
-                    {car.brand ? <span>merk: {car.brand}</span> : null}
-                    {car.type ? <span>type: {car.type}</span> : null}
-                    {car.color ? <span style={{background: colorMap.get(car.color), color: colorMap.get(car.color) === 'black' ? 'white' : ''}}>kleur: {car.color}</span> : null}
-                </Card>
-            </Col>)}
-        />
-        );
+    return <Section title={title} content={cars.map(car => <Car key={car.id} car={car}/>)}/>;
 }
--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,16 +1,17 @@
 import {Card} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import {Section} from "./Section";
+import * as PropTypes from "prop-types";
+
+function Number(props) {
+    return <Col className="g-3" xs={3}>
+        <Card>{props.n}</Card>
+    </Col>;
+}
+
+Number.propTypes = {n: PropTypes.any};
 
 export function Numbers(props) {
     const {title, numbers} = props;
-    return (
-        <Section
-        title={title}
-        content={
-            numbers.map((n, i) =>
-            <Col className='g-3' xs={3} key={i}>
-                <Card >{n}</Card>
-            </Col>)}
-        />);
+    return <Section title={title} content={numbers.map((n, i) => <Number key={i} n={n}/>)}/>;
 }
