48da76b89890ebc3fffd4b58089f86394027daf5
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-09-29 09:31:42 +0200

    added Persons in PersonsPage

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,8 +1,18 @@
 import {Section} from "./Section";
+import {MyCard} from "./MyCard";
 
 export function Persons(props) {
     const {persons, title} = props;
     return <Section title={title}>
-
+        {persons.map(p => <Person person={p} key={p.name}/>)}
     </Section>
+}
+
+function Person(props) {
+    const {person} = props;
+    return <MyCard>
+        <h1>{person.name}</h1>
+        <span>{person.age}</span>
+        <span>{person.city}</span>
+    </MyCard>
 }
