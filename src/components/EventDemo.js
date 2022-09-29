2ee2661f29bf72143e82d5e59b859a2ea993117f
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-09-29 11:30:44 +0200

    EventDemo

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -1,14 +1,22 @@
 import {Section} from "./Section";
 import {Button} from "react-bootstrap";
 
-export function EventDemo(props) {
+export function EventDemo() {
     return <Section title='events'>
         <EventButton />
         <BootstrapButton />
+        <div className='bg-danger text-start' onClick={() => alert('div is clicked')}>
+            Dit is een div<br/>
+            <a onClick={() => alert('een alert')} href='#'>
+                dit is een link met een href
+            </a>
+            -en-
+            <button onClick={(e) => {alert('button is clicked'); e.stopPropagation()}}>Dit is een button</button>
+        </div>
     </Section>
 }
 
-function EventButton(props) {
+function EventButton() {
     return <button onClick={() => alert('button is clicked')}>Click me Please!</button>
 }
 
