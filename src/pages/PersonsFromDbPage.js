b2c949ee40806bf585e69421a25611f25ec5b19b
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-10-17 16:54:43 +0200

    added PersonsFromDb search

--- src/pages/PersonsFromDbPage.js
+++ src/pages/PersonsFromDbPage.js
@@ -1,14 +1,21 @@
 import {collection} from 'firebase/firestore';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
+import {useCollectionData, useCollectionDataOnce} from 'react-firebase-hooks/firestore';
 import {firestoreDB} from '../services/firebase';
 import {Persons} from "../components/Persons";
+import {useState} from "react";
+import {MyInput} from "../components/MyInput";
 
 export function PersonsFromDbPage() {
     const query = collection(firestoreDB, 'Persons').withConverter(personConverter);
     const [values, loading, error] = useCollectionData(query);
     console.log({values, loading, error});
+    const [search, setSearch] = useState('');
     return (
-        <Persons title='persons from db' persons={values} open={true} />
+        <>
+            <Persons title='persons from db' persons={values?.filter(p => p.name.toLowerCase().match(search.toLowerCase()))} open={true} />
+            <MyInput label='Search:' onChange={(e) => setSearch(e.target.value)} />
+        </>
+
     );
 }
 
